77774f265090c42854e5fd54702337767602c4c2
root <root@hmk>
2025-05-25 17:58:04 +0900

    update server

--- client/main.c
+++ client/main.c
@@ -1,16 +1,50 @@
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
 #include <assert.h>
+#include <unistd.h>
+
+#define MAX_SIZE    1024
 
 int main() 
 {
-    int sockfd;
-    
-    if((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+    int client_sockfd;
+    struct sockaddr_in client_sockaddr;
+    char buf[MAX_SIZE] = {0,};
+    int length = 0;
+
+    if((client_sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
         assert(0);
     }
 
+    // Connect
+    memset(&client_sockaddr, 0, sizeof(client_sockaddr));
+    client_sockaddr.sin_family = AF_INET;
+    client_sockaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    client_sockaddr.sin_port = htons(8080);
+    
+    if(-1 == connect(client_sockfd, (struct sockaddr*)&client_sockaddr, sizeof(client_sockaddr))) {
+        perror("connect error: ");
+        return -1;
+    }
 
+    read(0, buf, MAX_SIZE);
+
+    if(write(client_sockfd, buf, MAX_SIZE) <= 0) {
+        perror("write error: ");
+        return -1;
+    }
+
+    if((length = read(client_sockfd, buf, MAX_SIZE)) <= 0) {
+        perror("read error: ");
+        return -1;
+    }
+
+    buf[length + 1] = '\0';
+    printf("read: %s\n", buf);
+    
+    close(client_sockfd);
     return 0;
 }
--- server/main.c
+++ server/main.c
@@ -1,15 +1,61 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
 #include <assert.h>
+#include <unistd.h>
 
-int main() 
+#define MAX_SIZE    1024
+
+int main(int argc, char* argv[]) 
 {
-    int sockfd;
+    int listen_sockfd;
+    int client_sockfd;
+    struct sockaddr_in server_addr;
     
-    if((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
-        assert(0);
+    // Create listen socket.
+    if((listen_sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+        perror("socket error: ");
+        return -1;   
     }
 
+    // Set server IP address and port number
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));   
+    bind(listen_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    
+    // Listen
+    listen(listen_sockfd, SOMAXCONN);
+
+    printf("Server Running...\n");
+
+    while(1) {
+        char buf[MAX_SIZE] = {0,};
+        int length = 0;
+
+        // Accept
+        struct sockaddr_in client_addr;
+        int addr_len = sizeof(client_addr);
+        client_sockfd = accept(listen_sockfd, (struct sockaddr*)&client_addr, &addr_len);
+
+        printf("New Client Connect: %s\n", inet_ntoa(client_addr.sin_addr));
+
+        if((length = read(client_sockfd, buf, MAX_SIZE)) <= 0) {
+            close(client_sockfd);
+            continue;
+        }
+
+        if(write(client_sockfd, buf, MAX_SIZE) <= 0) {
+            perror("write error: ");
+            close(client_sockfd);
+        }
+        
+        close(client_sockfd);
+    }
+
+    close(listen_sockfd);
     return 0;
 }
